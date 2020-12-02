1831abd137d86adcdaddc20ab3405a2e53c0684b
Tobbas <gustobbe@gmail.com>
2020-12-02 09:25:40 +0100

    Add comments

--- day2.py
+++ day2.py
@@ -4,10 +4,10 @@ test = ['1-3 a: abcde','1-3 b: cdbefg','2-9 c: acaaaaaaca']
 def solve(data):
     count = 0
     for p in data:
-        p = p.split()
-        a = p[0].split('-')
-        pol = re.findall(f'{p[1][0]}', p[2])
-        if int(a[0]) <= len(pol) <= int(a[1]):
+        p = p.split()                           # split on whitespace to separate parts of input
+        a = p[0].split('-')                     # extract numbers separated by -
+        pol = re.findall(f'{p[1][0]}', p[2])    # create list of all ocurrences
+        if int(a[0]) <= len(pol) <= int(a[1]):  # compare numbers with length of resulting list from regexp above
             count += 1
     return count
 
@@ -16,7 +16,7 @@ def solve2(data):
     for p in data:
         p = p.split()
         a = [int(i) for i in p[0].split('-')]
-        pwd = p[2]
+        pwd = p[2]                              # for readability, extract password string and control character in own variables
         char = p[1][0] 
         if a[1] <= len(pwd):
             if pwd[a[0]-1] == char and pwd[a[1]-1] != char:
